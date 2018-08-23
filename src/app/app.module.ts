c94f81c55e6d770f180beddbc3c34bd0eaa0067e
Ganesh Khadsan <ganesh_khadsan@persistent.co.in>
2018-08-23 15:33:46 +0530

    inital html commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainbodyComponent } from './mainbody/mainbody.component';
+import { StudentService } from './student.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainbodyComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [StudentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
